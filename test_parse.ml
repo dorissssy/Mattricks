fa4b26047148b4688857657989050a5533326f84
田源翰（Yuanhan Tian） <46698520+tiany7@users.noreply.github.com>
2022-11-20 11:37:56 +0800

    Update test_parse.ml

--- test_parse.ml
+++ test_parse.ml
@@ -1,7 +1,7 @@
 open Ast
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-  let program = Nanocparse.program_rule Scanner.token lexbuf in
+  let program = Parse.program_rule Scanner.token lexbuf in
   print_endline (string_of_program program)
 
 

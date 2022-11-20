475338860115f39c61ac03dd27d7ecd85b426501
Ubuntu <azureuser@GRPC.oi0j5qtqtgkejlgipl3agyl4qf.bx.internal.cloudapp.net>
2022-11-20 03:32:36 +0000

    latest push

--- test_semant.ml
+++ test_semant.ml
@@ -2,7 +2,7 @@ open Sast
 
 let _ =
           let lexbuf = Lexing.from_channel stdin in
-            let program = Nanocparse.program_rule Scanner.token lexbuf in
+            let program = Parse.program_rule Scanner.token lexbuf in
               let sprogram = Semant.check program in
                 print_endline (string_of_sprogram sprogram)
 
